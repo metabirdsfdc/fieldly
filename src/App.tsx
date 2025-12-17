c1524e6e82b73a301e30b6cd9c877cb62ff3cee3
metabirdsfdc <aquasensethefuture@gmail.com>
2025-12-17 12:57:59 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import DeployPage from "./pages/DeployPage";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-white text-gray-900">
-      <DeployPage />
-    </div>
-  );
+  return <DeployPage />;
 }
